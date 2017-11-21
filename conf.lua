79517dc18927fcb46a7ecc2730bb4b6067b90a3b
Mick McCarthy <mccarthy.mick0012@gmail.com>
2017-11-21 21:45:18 +0000

    Falling bird

--- conf.lua
+++ conf.lua
@@ -1,4 +1,5 @@
 function love.conf(t)
     t.window.width = 640
     t.window.height = 480
+    t.window.title = "Flappy Bird"
 end
--- main.lua
+++ main.lua
@@ -1,11 +1,19 @@
 function love.load()
-
+    bird = {}
+    bird.y = 200
+    bird.ySpeed = 0
+    g = 500
 end
 
 function love.update(dt)
-
+    bird.ySpeed = bird.ySpeed + (g * dt)
+    bird.y = bird.y + (bird.ySpeed * dt)
 end
 
 function love.draw()
+    love.graphics.setColor(31, 97, 127)
+    love.graphics.rectangle('fill', 0, 0, love.graphics.getWidth(), love.graphics.getHeight())
 
+    love.graphics.setColor(255, 215, 63)
+    love.graphics.rectangle('fill', 65, bird.y, 30, 25)
 end
